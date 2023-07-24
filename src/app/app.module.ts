8b8aab1ee439fc7f0c3ddab510ed2c0ba8b135c4
Michelle Broens <michellebroens96@gmail.com>
2023-07-24 13:23:30 +0200

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { WolfListComponent } from './components/wolf-list/wolf-list.component';
 import { PackListComponent } from './components/pack-list/pack-list.component';
 import { PackMapComponent } from './components/pack-map/pack-map.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WolfListComponent,
     PackListComponent,
-    PackMapComponent
+    PackMapComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
